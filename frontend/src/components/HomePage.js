025fde963e26195b2d8b81d710185418116235ae
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2022-03-21 18:16:46 -0700

    Added Room Component and get_room() details

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useParams, useNavigate, Link, Redirect } from 'react-router-dom';
 import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
+import Room from './Room';
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -9,12 +10,24 @@ export default class HomePage extends Component {
     }
 
     render() {
+
+        const RoomWrapper = (props) => {
+            const params = useParams();
+            return <Room {...{...props, match: {params}} } />;
+        }
+
+        const CreateRoomWrapper = (props) => {
+            const history = useNavigate();
+            return <CreateRoomPage {...{...props, history }} />;
+        }
+
         return (
             <Router>
                 <Routes>
                     <Route exact path="/" element={<div>This is the Home Page</div>} />
                     <Route path="/join" element={<RoomJoinPage />} />
-                    <Route path="/create-room" element={<CreateRoomPage />} />
+                    <Route path="/create-room" element={<CreateRoomWrapper />} />
+                    <Route path="/room/:roomCode" element={<RoomWrapper />} />
                 </Routes>
             </Router>
         );
