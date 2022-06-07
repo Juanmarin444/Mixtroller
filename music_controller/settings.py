fa8d026c548f34710bc43ad2c11e1f6294239c2e
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2022-06-06 17:37:25 -0700

    Test

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -137,6 +137,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'frontend/static'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
