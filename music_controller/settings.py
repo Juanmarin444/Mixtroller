3416aa2fc8cb2eae6f4312e40fc6a0535d448fd2
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2022-06-06 18:03:28 -0700

    test

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -138,9 +138,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend/static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'frontend/static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
