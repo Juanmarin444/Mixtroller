3e97e2a8fe9c94ebda5d90ccce778b4d174a1643
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2022-06-06 17:45:01 -0700

    test

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
     'frontend.apps.FrontendConfig',
